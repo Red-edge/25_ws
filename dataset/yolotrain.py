421bd3bc39e1ac0452b5c246dc9289dcadd3bc07
Rededge-mac <2061534353@qq.com>
2025-12-13 19:02:20 +0800

    trainning

--- dataset/yolotrain.py
+++ dataset/yolotrain.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with MPS
-results = model.train(data="data.yaml", epochs=100, imgsz=640, device="mps")
+results = model.train(data="data.yaml", epochs=200, imgsz=640, device="mps")
